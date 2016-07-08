fb0a0b1a179a84961e5a992fe3183e6c67f65dce
PengYunjing <137813369@qq.com>
2016-07-08 10:59:30 +0800

    Initial Commit

---
+++ 贷款计算器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  贷款计算器
+//
+//  Created by Apple on 16/7/8.
+//  Copyright © 2016年 PYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 贷款计算器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  贷款计算器
+//
+//  Created by Apple on 16/7/8.
+//  Copyright © 2016年 PYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
